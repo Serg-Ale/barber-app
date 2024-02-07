2d7193d3bb25f6104690a5a27c99800e9b6991f6
Serg-Ale <sergioalexandre0716@gmail.com>
2024-02-07 16:45:36 -0300

    feat: changed the name of some files

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/Search";
