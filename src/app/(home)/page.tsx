19167fb83082fd586933d8a800a071c8df0262d8
Serg-Ale <sergioalexandre0716@gmail.com>
2024-02-07 16:06:10 -0300

    feat: add 'Olá, usuário' component to the homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,20 @@
 import Header from "@/components/Header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, usuário!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 }
