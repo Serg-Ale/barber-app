2d2e70370c80609238a09350521b573725a16d23
Serg-Ale <sergioalexandre0716@gmail.com>
2024-02-17 18:56:19 -0300

    feat: add initial Footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="w-full bg-secondary py-6 px-5 justify-center text-center">
+      <p className="text-gray-400 text-xs font-bold opacity-75">
+        sergioalexandre0716@gmail.com
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
