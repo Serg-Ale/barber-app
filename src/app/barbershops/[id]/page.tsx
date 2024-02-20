b75b0c74eaefe80201e8d840d2b83552d096e0b9
Serg-Ale <sergioalexandre0716@gmail.com>
2024-02-20 06:16:14 -0300

    feat: add initial service booking

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -39,6 +39,7 @@ const BarbershopDetailsPage = async ({
           <ServiceItem
             key={service.id}
             service={service}
+            barbershop={barbershop}
             isAuthenticated={!!session?.user}
           />
         ))}
