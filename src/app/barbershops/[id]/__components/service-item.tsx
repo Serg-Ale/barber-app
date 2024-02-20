ccd64a5767875aa30bab73f39e3bebfce57a4da9
Serg-Ale <sergioalexandre0716@gmail.com>
2024-02-20 10:03:03 -0300

    feat: ensure only available hour are shown on service booking

--- src/app/barbershops/[id]/__components/service-item.tsx
+++ src/app/barbershops/[id]/__components/service-item.tsx
@@ -44,19 +44,17 @@ const ServiceItem = ({
   const [sheetIsOpen, setSheetIsOpen] = useState(false);
   const [dayBookings, setDayBookings] = useState<Booking[]>([]);
 
-  console.log({ dayBookings });
-
   useEffect(() => {
     if (!date) return;
 
     const refreshAvailableHours = async () => {
-      const _dayBookings = await getDayBookings(date);
+      const _dayBookings = await getDayBookings(barbershop.id, date);
 
       setDayBookings(_dayBookings);
     };
 
     refreshAvailableHours();
-  }, [date]);
+  }, [date, barbershop.id]);
 
   const handleDateClick = (date: Date | undefined) => {
     setDate(date);
